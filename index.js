316f983e438df2480bb74de843a852bee913d3ed
Blessing <bntoko@gmail.com>
2020-09-12 11:37:19 +0200

    changed state array

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
-let temp = document.getElementsByTagName('g');
-let tempArray = [];
-
-for (let i = 0; i < temp.length; i++) {
-    tempArray[i] = temp[i].id;
-}
-
-console.log(tempArray);
+let stateArray = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Washington_DC', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'NewHampshire', 'NewJersey', 'NewMexico', 'NewYork', 'NorthCarolina', 'NorthDakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'RhodeIsland', 'SouthCarolina', 'SouthDakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'WestVirginia', 'Wisconsin', 'Wyoming'];
 
 let results1996 = [0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1, 0];
 let results2000 = [0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0];
@@ -39,7 +32,15 @@ function get2016Result() {
 }
 
 function changeColors(arrayYear) {
-    for (i = 0; i < tempArray; i++) {
-        
+    for (i = 0; i < stateArray.length; i++) {
+        let stateElement = document.getElementById(stateArray[i]);
+        for (j = 0; j < stateElement.childElementCount; j++) {
+            if (arrayYear[i]) {
+                stateElement.children[j].setAttribute('style', 'fill: #367dcf');
+            } else {
+                stateElement.children[j].setAttribute('style', 'fill: #a92b17');
+
+            }
+        }
     }
 }
